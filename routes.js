b1e4490ec4b773a80220fe03ab441e3c706856e8
Patricia Mic <micpatricia97@gmail.com>
2019-04-15 15:38:47 +0300

    ryggre

--- routes.js
+++ routes.js
@@ -42,7 +42,7 @@ async function getLatestPhoto(ctx) {
 
 async function getAllPosts(ctx) {
     let mdata = ctx.request.body;
-    let res = await pool.executeQuery("select username, idUser, photo, description from posts join users on posts.idUser= users.id where idUser!=" + mdata.idUser);
+    let res = await pool.executeQuery("select username, idUser, photo, description, code from posts join users on posts.idUser= users.id where idUser!=" + mdata.idUser);
     let resStringfy = JSON.stringify(res);
     console.log(resStringfy);
     ctx.body = { message: `${resStringfy}` };
