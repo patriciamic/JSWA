2587d05a93eaa64046a72006b0f4c28bb9a5b981
Patricia Mic <micpatricia97@gmail.com>
2019-04-19 11:22:58 +0300

    adding fcm. works on localhost

--- client/index.js
+++ client/index.js
@@ -15,22 +15,12 @@ let token = null;
 
 messaging.usePublicVapidKey('BAp7u4PzjypiDrdRUU2JX4nCWImGV66jKQKnzC1_MkdtH5oYcCAq9gB0aqAHg-XpfoHoj505_ADomduEvNSWrko');
 
-messaging.requestPermission().then(() => {
-    console.log('Permission granted.');
-
-    return messaging.getToken()
-        .then(function(currentToken) {
-            console.log(currentToken);
-            token = currentToken;
-        }).catch(function(err) {
-            console.log('An error occurred while retrieving token. ', err);
-            showToken('Error retrieving Instance ID token. ', err);
-            setTokenSentToServer(false);
-        });
-}).catch(err => console.log('aici:', err));
+messaging.requestPermission()
+    .then(() => messaging.getToken())
+    .then(tokenS => token = tokenS)
+    .catch(err => console.log('Err :', err));
 
 messaging.onMessage(payload => {
-    console.log(payload);
     let obj = payload.notification;
     new Notification(obj.title, {
         body: obj.body
